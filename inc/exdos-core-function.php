f9eba7ff1033a8cdaa4e7c7edda8010e4a946122
Turjo884 <hturjokhan@gmail.com>
2025-01-31 18:54:19 +0600

    done post wiedget

--- inc/exdos-core-function.php
+++ inc/exdos-core-function.php
@@ -1,5 +1,35 @@
 <?php
 
+// get all category 
+function tp_post_cat($category = 'category'){
+   $categories = get_categories( array(
+         'taxonomy' => $category,
+         'orderby' => 'name',
+         'order'   => 'ASC',
+         'posts_per_page'   => -1,
+   ) );
+   $cat_list = [];
+   foreach($categories as $cat){
+         $cat_list[$cat->slug] = $cat->name;
+   }
+   return $cat_list;
+}
+
+// get all post 
+function tp_all_post($post_type_name = 'post'){
+   $posts = get_posts( array(
+         'post_type' => $post_type_name,
+         'orderby' => 'name',
+         'order'   => 'ASC',
+         'posts_per_page'   => -1,
+   ) );
+   $posts_list = [];
+   foreach($posts as $post){
+         $posts_list[$post->ID] = $post->post_title;
+   }
+   return $posts_list;
+}
+
 /**
 * Sanitize SVG markup for front-end display.
 *
