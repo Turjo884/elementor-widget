6ed82089c5dd6f31eadfb9f738c435a74b7dacdc
Turjo884 <hturjokhan@gmail.com>
2025-04-13 23:58:10 +0600

    working on one click demo data importing and other stuff

--- widgets/pricing-table.php
+++ widgets/pricing-table.php
@@ -25,7 +25,7 @@ class Exdos_Pricing_Table extends Widget_Base {
 	 * @return string Widget name.
 	 */
 	public function get_name() {
-		return 'exdos-pricing-table';
+		return 'exdos-prcing-table';
 	}
 
 	/**
@@ -107,65 +107,41 @@ class Exdos_Pricing_Table extends Widget_Base {
 
 	//  register controls section
 	protected function register_controls_section() {
-
-		//  Start Image Section
-		$this->start_controls_section(
-			'pricing_image_section',
-			[
-				'label' => esc_html__( 'Image', 'textdomain' ),
-				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-			]
-		);
-
-		$this->add_control(
-			'pricing_image',
-			[
-				'label' => esc_html__( 'Choose Image', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
-				],
-			]
-		);
-
-		$this->add_control(
-			'bg_image',
-			[
-				'label' => esc_html__( 'Choose Image', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
-				],
-			]
-		);
-
-		$this->end_controls_section();
-		// End Image Section
-
 		// Start Text Section
 		$this->start_controls_section(
-			'services_section_content',
+			'pricing_section_content',
 			[
-				'label' => __( 'Pricing Content', 'exdos-core' ),
+				'label' => __( 'Price Content', 'exdos-core' ),
 			]
 		);
 
 		$this->add_control(
-			'price_title',
+			'main_title',
 			[
-				'label' => __( 'Price', 'exdos-core' ),
+				'label' => __( 'Main Title', 'exdos-core' ),
 				'type' => Controls_Manager::TEXT,
 				'default' => esc_html__( 'Main Tittle One', 'textdomain' ),
 				'label_block' => true,
 			]
 		);
 
+
 		$this->add_control(
-			'description',
+			'price-sign',
 			[
-				'label' => __( 'Content', 'exdos-core' ),
-				'type' => Controls_Manager::TEXTAREA,
-				'default' => esc_html__( 'Perfs ipsum ultrices sollicitudin iaculis platea facilisi', 'textdomain' ),
+				'label' => __( 'Price Sign', 'exdos-core' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => esc_html__( '$', 'textdomain' ),
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'price',
+			[
+				'label' => __( 'Price', 'exdos-core' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => esc_html__( '120', 'textdomain' ),
 				'label_block' => true,
 			]
 		);
@@ -174,51 +150,33 @@ class Exdos_Pricing_Table extends Widget_Base {
 		// End Text Section
 
 
-		// start repeater control
+		// Start Icon Section 
 		$this->start_controls_section(
-			'prices_list_section',
+			'price_icon_section',
 			[
-				'label' => esc_html__( 'Pricing List', 'textdomain' ),
+				'label' => esc_html__( 'Icon', 'textdomain' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
 
-		$repeater = new \Elementor\Repeater();
-
-		$repeater->add_control(
-			'prices_list_name',
-			[
-				'label' => esc_html__( 'List Item', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Branding Design' , 'textdomain' ),
-				'label_block' => true,
-			]
-		);
-
 		$this->add_control(
-			'price_feature_list',
+			'icon',
 			[
-				'label' => esc_html__( 'Pricing Feature List', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::REPEATER,
-				'fields' => $repeater->get_controls(),
+				'label' => esc_html__( 'Icon', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::ICONS,
 				'default' => [
-					[
-						'prices_list_name' => esc_html__( 'prices_list_name', 'textdomain' ),
-					],
-					[
-						'prices_list_name' => esc_html__( 'Research & Testing', 'textdomain' ),
-					],
+					'value' => 'fas fa-circle',
+					'library' => 'fa-solid',
 				],
-				'title_field' => '{{{ prices_list_name }}}',
 			]
 		);
 
 		$this->end_controls_section();
-
+		// End Section 
 
 		// Start Button Section
 		$this->start_controls_section(
-			'hero_button_content',
+			'price_button_section',
 			[
 				'label' => __( 'Button', 'exdos-core' ),
 			]
@@ -252,6 +210,48 @@ class Exdos_Pricing_Table extends Widget_Base {
 
 		$this->end_controls_section();
 		// End Button Section
+
+
+		// start repeater control
+		$this->start_controls_section(
+			'price_list_section',
+			[
+				'label' => esc_html__( 'Features List', 'textdomain' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$repeater = new \Elementor\Repeater();
+
+		$repeater->add_control(
+			'list_name',
+			[
+				'label' => esc_html__( 'List Item', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Branding Design' , 'textdomain' ),
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'features_list',
+			[
+				'label' => esc_html__( 'Feature List', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => $repeater->get_controls(),
+				'default' => [
+					[
+						'list_name' => esc_html__( 'Full design support', 'textdomain' ),
+					],
+					[
+						'list_name' => esc_html__( 'Customizable registration', 'textdomain' ),
+					],
+				],
+				'title_field' => '{{{ list_name }}}',
+			]
+		);
+
+		$this->end_controls_section();
 		
 	}
 
@@ -304,138 +304,46 @@ class Exdos_Pricing_Table extends Widget_Base {
 
 		if ( ! empty( $settings['exdos_button'] ) ) {	
 			$this->add_link_attributes( 'button_arg', $settings['exdos_button_url'] );
-			$this->add_render_attribute('button_arg', 'class', 'tp-sv-btn br-5');
+			$this->add_render_attribute('button_arg', 'class', 'tp-btn-orange');
 		}	
 
 		?>
 
-		<!-- Start Pricing Box -->
 
-		<section class="tp-pricing-area pt-120 pb-90">
-            <div class="container">
-                <!-- <div class="tp-section-title-wrapper mb-50 text-center">
-                    <h2 class="tp-section-title mb-20">Price table</h2>
-                    <p>Per ipsum ultrices sollicitudin iaculis platea facilisi semper aliquam up <br> senectus cursus vivamus volutpat penatibus</p>
-                </div>
-                <div class="row"> -->
-                    <div class="col-xl-4 col-lg-6 col-md-6">
-                        <div class="tpprice mb-30 pt-60 pb-60 pl-50 pr-50 wow tpFadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-bg-color="#FDF6F2" style="background-image: url('<?php echo esc_url($settings['bg_image']['url']); ?>');">
-                            <div class="tpprice__icon">
-                                <span><i class="flaticon-geometric-6"></i></span>
+        <!-- Start Pricing Table -->
+		<div class="tpprice mb-30 pt-60 pb-60 pl-50 pr-50 wow tpFadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-bg-color="#FDF6F2" data-background="assets/img/price/price-bg.png">
+			<div class="tpprice__icon">
+				<span><?php \Elementor\Icons_Manager::render_icon( $settings['icon'], [ 'aria-hidden' => 'true' ] ); ?></span>
+				<div class="tpprice__icon-shape">
+					<img src="<?php echo get_template_directory_uri();?>/assets/img/price/shape-1.png" alt="">
+				</div>
+			</div>
+			<div class="tpprice__price">
+				<h4><span><?php echo exdos_core_kses($settings['price-sign'])?></span><?php echo exdos_core_kses($settings['price'])?></h4>
+			</div>
+			
+			<h3 class="tpprice__title"><?php echo exdos_core_kses($settings['main_title']); ?></h3>
+			<div class="tpprice__sep"></div>
+			
+			<ul class="tpprice__features">
+			<?php foreach($settings['features_list'] as $item) : ?>
+				<li><i class="fal fa-check"></i> <span><?php echo esc_html($item['list_name']); ?></span></li>
+			<?php endforeach; ?>
+			</ul>
 
-								<?php if(!empty($settings['exdos_image']['url'])) : ?>
-                                <div class="tpprice__icon-shape">
-                                    <img src="<?php echo esc_url($settings['exdos_image']['url']); ?>" alt="">
-                                </div>
-								<?php endif; ?>
-
-                            </div>
-
-							<?php if(!empty($settings['price_title'])) : ?>
-                            <div class="tpprice__price">
-                                <h4><?php echo exdos_core_kses($settings['price_title'])?></h4>
-                            </div>
-							<?php endif; ?>
-
-
-							<?php if(!empty($settings['description'])) : ?>
-                            <h3 class="tpprice__title"><?php echo exdos_core_kses($settings['description'])?></h3>
-							<?php endif; ?>
-
-
-                            <div class="tpprice__sep"></div>
-                            <ul class="tpprice__features">
-
-							<?php foreach($settings['price_feature_list'] as $item) : ?>
-                                <li><i class="fal fa-check"></i> <span><?php echo esc_html($item['prices_list_name']); ?></span></li>
-							<?php endforeach; ?>
-                                
-                            </ul>
-                            <div class="tp-price-btn mt-50">
-                                <a <?php echo $this->get_render_attribute_string('button_arg'); ?>>
-                                    <span class="tp-btn-wrap">
-                                        <span class="tp-btn-y-1"><?php echo esc_html($settings['exdos_button']); ?></span>
-                                        <span class="tp-btn-y-2"><?php echo esc_html($settings['exdos_button']); ?></span>
-                                    </span>  
-                                    <i></i> 
-                                </a>
-								
-                            </div>
-                        </div>
-                    </div>
-                    <!-- <div class="col-xl-4 col-lg-6 col-md-6">
-                        <div class="tpprice mb-30 pt-60 pb-60 pl-50 pr-50 wow tpFadeInUp" data-wow-duration="1s" data-wow-delay="0.4s" data-bg-color="#FDF6F2" data-background="assets/img/price/price-bg.png">
-                            <div class="tpprice__icon">
-                                <span><i class="flaticon-geometric-7"></i></span>
-                                <div class="tpprice__icon-shape">
-                                    <img src="assets/img/price/shape-2.png" alt="">
-                                </div>
-                            </div>
-                            <div class="tpprice__price">
-                                <h4><span>$</span>150</h4>
-                            </div>
-                            
-                            <h3 class="tpprice__title">App development</h3>
-                            <div class="tpprice__sep"></div>
-                            <ul class="tpprice__features">
-                                <li><i class="fal fa-check"></i> <span> Full design support</span></li>
-                                <li><i class="fal fa-check"></i> <span> Customizable registration</span></li>
-                                <li><i class="fal fa-check"></i> <span> Guarantee Approval</span></li>
-                                <li><i class="fal fa-check"></i> <span> Custom domain</span></li>
-                                <li><i class="fal fa-check"></i> <span> Unlimited paid ticket</span></li>
-                                <li><i class="fal fa-check"></i> <span>24/7 Client support </span></li>
-                            </ul>
-                            <div class="tp-price-btn mt-50">
-                                <a href="contact.html" class="tp-btn-orange">
-                                    <span class="tp-btn-wrap">
-                                        <span class="tp-btn-y-1">Choose a Plan</span>
-                                        <span class="tp-btn-y-2">Choose a Plan</span>
-                                    </span>  
-                                    <i></i> 
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-6 col-md-6">
-                        <div class="tpprice mb-30 pt-60 pb-60 pl-50 pr-50  wow tpFadeInUp" data-wow-duration="1s" data-wow-delay="0.6s" data-bg-color="#FDF6F2" data-background="assets/img/price/price-bg.png">
-                            <div class="tpprice__icon">
-                                <span><i class="flaticon-geometric-8"></i></span>
-                                <div class="tpprice__icon-shape">
-                                    <img src="assets/img/price/shape-3.png" alt="">
-                                </div>
-                            </div>
-                            <div class="tpprice__price">
-                                <h4><span>$</span>180</h4>
-                            </div>
-                            
-                            <h3 class="tpprice__title">Digital marketing</h3>
-                            <div class="tpprice__sep"></div>
-                            <ul class="tpprice__features">
-                                <li><i class="fal fa-check"></i> <span> Full design support</span></li>
-                                <li><i class="fal fa-check"></i> <span> Customizable registration</span></li>
-                                <li><i class="fal fa-check"></i> <span> Guarantee Approval</span></li>
-                                <li><i class="fal fa-check"></i> <span> Custom domain</span></li>
-                                <li><i class="fal fa-check"></i> <span> Unlimited paid ticket</span></li>
-                                <li><i class="fal fa-check"></i> <span>24/7 Client support </span></li>
-                            </ul>
-                            <div class="tp-price-btn mt-50">
-                                <a href="contact.html" class="tp-btn-orange">
-                                    <span class="tp-btn-wrap">
-                                        <span class="tp-btn-y-1">Choose a Plan</span>
-                                        <span class="tp-btn-y-2">Choose a Plan</span>
-                                    </span>  
-                                    <i></i> 
-                                </a>
-                            </div>
-                        </div>
-                    </div> -->
-                </div>
-            </div>
-        </section>
-
-		<!-- End Pricing Box -->
-
-
+			<?php if(!empty($settings['exdos_button'])) : ?>
+			<div class="tp-price-btn mt-50">
+				<a <?php echo $this->get_render_attribute_string('button_arg'); ?>>
+					<span class="tp-btn-wrap">
+						<span class="tp-btn-y-1"><?php echo esc_html($settings['exdos_button']); ?></span>
+						<span class="tp-btn-y-2"><?php echo esc_html($settings['exdos_button']); ?></span>
+					</span>  
+					<i></i> 
+				</a>
+			</div>
+			<?php endif; ?>
+		</div>
+		<!-- End Pricing Table -->
 
 
 		<?php
